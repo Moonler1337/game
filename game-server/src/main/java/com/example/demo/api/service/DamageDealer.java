13023c1eb65c0921d5d9b77b6e0c000c16e24543
maksim <Moonl3rr@gmail.com>
2026-02-24 11:58:58 +0400

    Почистил лишнее: убрал мусорные поля в сервисах и лишний starter из pom

--- game-server/src/main/java/com/example/demo/api/service/DamageDealer.java
+++ game-server/src/main/java/com/example/demo/api/service/DamageDealer.java
@@ -4,14 +4,4 @@ import org.springframework.stereotype.Service;
 
 @Service
 public class DamageDealer {
-
-    String actorId;
-    String targetId;
-    int value;
-    String actorName;
-    String email;
-    String ID_REGEX;
-
-
-
 }
--- game-server/src/main/java/com/example/demo/api/service/ServiceForTest.java
+++ game-server/src/main/java/com/example/demo/api/service/ServiceForTest.java
@@ -4,9 +4,6 @@ import org.springframework.stereotype.Service;
 
 @Service
 public class ServiceForTest {
-    int first;
-    int second;
-
     public int plus(int first, int second) {
         return first + second;
     }
